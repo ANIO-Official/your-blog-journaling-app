a9c6958a8f2a0fc91d47b1101d12e241a1d2917a
Amanda Ogletree <amandaogletree19@gmail.com>
2025-11-09 12:09:02 -0500

    Added function renderFeed() to add items dynamically to webpage via document fragment batch. Added event listener to form to check validity before adding posts upon submission. Added README file.

---
+++ README.md
@@ -0,0 +1,56 @@
+# Module 5 SBA The Document Object Model
+## SBA Overview
+The purpose of this project is to test my ability to manipulate the DOM, handle user events, implement form validation, and utilize localStorage for data persistence. Users should be able to:
+
+- Create New Posts
+- Read/View Displayed Posts
+- Update/Edit Posts
+- Delete Posts
+- See previous (undeleted) post from previous session on reload.
+------------------------------------------------
+## Table of contents
+
+- [Overview](#overview)
+  - [GitHub](#github)
+  - [How To Use](#how-to-use)
+  - [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+### GitHub Link
+
+### How To Use
+
+## My process
+Quick brief about process. Here are my steps/general steps:
+ 1. Set up Semantic HTML for Web App structure
+
+  - Bullet Points
+
+ 2. Create Responsive Layout using Bootstrap Grid and CSS
+
+  - Bullet Points
+
+ 3. Added DOM Variables and Validity Conditional Checks
+
+ 4. Added Functions to Dynamically Create Blog Post upon Submission
+
+ 5. Testing and Bug Fixing
+
+ 6. Styling Tweaks
+
+### Built with
+- JavaScript
+- Semantic HTML
+- CSS
+- BootStrap/Other CSS Libraries
+
+
+### Useful resources
+
+- [MDN <template>](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/template) - I wanted to read more about templating to understand how to dynamically change the content per entry. As I read this document, I saw a wonderful example under 'Generating Table Rows'. It showed that I could use querySelector on parent elements! This has opened new doors for me. :'D
+
+## Author
+
+- LinkedIn - [Amanda Ogletree](https://www.linkedin.com/in/amanda-ogletree-a61b60168)
--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //Global & DOM Variables
+//Blog Post & Feed--------
+let feedBatch = document.getElementById('feed-batch')
+let blogItemTemplate = document.querySelector('.blog-item-template')
+let feedData = []
+
 
 //Form---------
 const createBlogForm = document.querySelector('#create-blog-form')
@@ -12,7 +17,9 @@ const blogContentInput = document.querySelector('#create-blog-content')
 const blogTitleError = document.querySelector('#title-error')
 const blogContentError = document.querySelector('#content-error')
 
-blogTitleInput.addEventListener('input', (event)=>{
+
+//Blog Form Field Validity Checks----------------------------------
+blogTitleInput.addEventListener('input', (event) => {
     if (blogTitleInput.validity.patternMismatch) {
         blogTitleInput.setCustomValidity('Must contain atleast 1 letter.')
     }
@@ -26,7 +33,7 @@ blogTitleInput.addEventListener('input', (event)=>{
     blogTitleError.textContent = blogTitleInput.validationMessage
 })
 
-blogContentInput.addEventListener('input', (event)=>{
+blogContentInput.addEventListener('input', (event) => {
     if (blogContentInput.validity.valueMissing) {
         blogContentInput.setCustomValidity("You haven't entered any content.")
     }
@@ -35,4 +42,99 @@ blogContentInput.addEventListener('input', (event)=>{
     }
     else { blogContentInput.setCustomValidity('') }
     blogContentError.textContent = blogContentInput.validationMessage
+})
+
+//Blog & Feed Creation----------------------------------
+
+//Event Delegation for each post, on the ul feedBatch (#feed-batch) 
+feedBatch.addEventListener('click', (event) => {
+    if (event.target.classList.contains('editBtn')) {
+        //Open modal or repopulate form to edit 
+    }
+    if (event.target.classList.contains('deleteBtn')) {
+        const blogItem = event.target.closest('li')
+        //Add a Removal from the local storage as well here.
+        blogItem.remove()
+    }
+
+})
+
+//Dynamically Add Posts to batch
+
+
+
+//Render Posts in Feed
+function renderFeed() {
+
+    //Create Fragment for batch--------------------------
+    const fragment = document.createDocumentFragment()
+
+    //Blog item Object Setup--------------------------
+    let item = {
+        id: Math.floor(Math.random() * 1000),
+        title: blogTitleInput.value,
+        content: blogContentInput.value,
+        timestamp: Date.now()
+    }
+
+    //Data Setup & Update--------------------------
+    if (localStorage.getItem('feedDisplayData')) { //Returning user
+        const retrievedFeedData = localStorage.getItem('feedDisplayData') //Get local storage array
+        feedData = JSON.parse(retrievedFeedData) //Update the array with stored data
+        feedData.push(item) //Add new blogItem into the array
+    }
+    else { //New users
+        feedData.push(item)
+    }
+
+    //Add Array to Fragment--------------------------
+    for (let blog of feedData) {
+        //Blog item Display variables
+        const blogItem = blogItemTemplate.cloneNode(true)
+        const blogTitle = blogItem.querySelector('.blog-item-title')//Get title display
+        const blogContent = blogItem.querySelector('.blog-item-content') //Get content display
+
+        //Display Setup--------------------------
+        blogItem.style.display = 'block'
+        blogItem.classList.remove('blog-item-template')//Remove template class.
+        blogItem.classList.add('blog-item') //New class to differentiate from template
+        blogTitle.textContent = blog.title //Set title display
+        blogContent.textContent = blog.content  // Set content display
+
+        fragment.appendChild(blogItem)
+    }
+
+    //Attach document fragment to feedBatch <ul>--------------------------
+    feedBatch.appendChild(fragment)
+}
+
+
+//Form Submission----------------------------------
+/*
+1. Check Validity Before Actions
+2. If invalid: Show Alert
+3. If Valid:
+    1. Submit the data into a new FormData
+    2. Call renderFeed() to show all blogItems with new blogItem added
+    3. Store the array of blogItems into localStorage
+    4. Clear Fields
+*/
+createBlogForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    //Stop creation of blog when invalid, display error alert.
+    const validityCheck = !blogTitleInput.validity.valid && !blogContentInput.validity.valid
+
+    if (validityCheck) {
+        alert('Invalid Blog. Check & updated highlighted field(s).')
+        return
+    }
+
+    //Create new blog with inputs if all valid
+    const newBlogData = new FormData(createBlogForm) //new form submission 
+    renderFeed() //New information pushed into array here
+    localStorage.setItem('feedDisplayData', JSON.stringify(feedData)) //Stringify array again for local storage
+    alert('New blog added to timeline (o゜▽゜)o☆')
+    createBlogForm.reset()
+
+
 })
